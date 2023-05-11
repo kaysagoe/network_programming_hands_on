e0cdd7649a0575a14e0f986aa160f16c553c5305
Kanyinsola Sagoe <ksagoe@gmail.com>
2023-05-11 20:24:00 +0100

    - use ethertype when creating sockets - extract interface_exists function - change read_frame to cater for ethertype value in frame

--- solutions/python/link_layer/ethernet_client_server/utils.py
+++ solutions/python/link_layer/ethernet_client_server/utils.py
@@ -12,14 +12,20 @@ class Frame:
     payload: bytes
 
 
+def interface_exists(name: str) -> bool:
+    if name not in [name_index[1] for name_index in socket.if_nameindex()]:
+        return False
+    return True
+
+
 def create_ether_sock(interface: str) -> Tuple[socket.socket, bytes]:
-    if interface not in [name_index[1] for name_index in socket.if_nameindex()]:
+    if not interface_exists(interface):
         raise Exception(f"Interface {interface} does not exist in the host")
 
     with open(f"/sys/class/net/{interface}/address", "rb") as address_file:
         mac = mac_hex_to_bin(address_file.read())
 
-    sock_fd = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)  # type: ignore
+    sock_fd = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x88B6))  # type: ignore
     sock_fd.bind((interface, 0))
 
     return sock_fd, mac
@@ -28,22 +34,23 @@ def create_ether_sock(interface: str) -> Tuple[socket.socket, bytes]:
 def send_frame(sock_fd: socket.socket, host_mac: bytes, dest_mac: bytes, payload: str):
     payload_bin = payload.encode("ascii")
     length = (14 + len(payload_bin)).to_bytes(2)
-    data = host_mac + dest_mac + length + payload.encode("ascii")
+    data = host_mac + dest_mac + b"\x88\xB6" + length + payload.encode("ascii")
     sock_fd.sendall(data)
 
 
 def read_frame(sock_fd: socket.socket, read_buffer: bytes) -> Tuple[Frame, bytes]:
-    if len(read_buffer) < 14:
+    if len(read_buffer) < 16:
         read_buffer = _read_to_buffer(read_buffer, sock_fd)
 
     host_mac, read_buffer = _get_bytes_from_buffer(read_buffer, 6)
     dest_mac, read_buffer = _get_bytes_from_buffer(read_buffer, 6)
+    ether_type, read_buffer = _get_bytes_from_buffer(read_buffer, 2)
     length, read_buffer = _get_bytes_from_buffer(read_buffer, 2)
     length_int = int.from_bytes(length)
 
     if len(read_buffer) < length_int:
         read_buffer = _read_to_buffer(read_buffer, sock_fd)
-    payload, read_buffer = _get_bytes_from_buffer(read_buffer, length_int - 14)
+    payload, read_buffer = _get_bytes_from_buffer(read_buffer, length_int - 16)
     return Frame(host_mac, dest_mac, length_int, payload), read_buffer
 
 
